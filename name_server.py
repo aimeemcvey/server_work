eafea950693afb7341e6b75086de5de682ee1a94
Aimee McVey <ajm111@duke.edu>
2020-03-20 19:24:35 -0400

    Adds student listing to server, calls sum

--- name_server.py
+++ name_server.py
@@ -2,15 +2,21 @@
 
 import requests
 
-r = requests.get("https://api.github.com/repos/aimeemcvey/BME547Class/branches")
-answer = r.json()
-print(answer)
-for branch in answer:
-    print("Name of branch is {}".format(branch["name"]))
+student = {"name": "Aimee McVey", "net_id": "ajm111", "e-mail": "aimee.mcvey@duke.edu"}
+# s = requests.post("http://vcm-7631.vm.duke.edu:5001/student", json=student)
+# if s.status_code == 200:
+#     print(s.json())
+# else:
+#     print("Error with status code of {}" .format(s.status_code))
 
-request_json = {"one": "Spain", "two": "Canada"}
-s = requests.post("http://vcm-7631.vm.duke.edu:5000/compare", json=request_json)
-if r.status_code == 200:
-    print(s.json())
+r = requests.get("http://vcm-7631.vm.duke.edu:5001/list")
+student_list = r.json()
+print(student_list)
+
+numbers = {"a": 6, "b": 2}
+n = requests.post("http://vcm-7631.vm.duke.edu:5001/sum", json=numbers)
+if n.status_code == 200:
+    print("The sum is {}." .format(n.json()))
 else:
-    print("Error with status code of {}" .format(r.status_code))
+    print("Error with status code of {}" .format(n.status_code))
+
