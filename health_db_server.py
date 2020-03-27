c0d3afac1b589a66ccdb8b20a062cdcba0b0cd74
Aimee McVey <aimee.mcvey@duke.edu>
2020-03-27 13:01:07 -0400

    Add procedure to initialize database before starting server

--- health_db_server.py
+++ health_db_server.py
@@ -1,9 +1,22 @@
 # health_db_server.py
-from flask import Flash, jsonify, request
+from flask import Flask, jsonify, request
 
 db = []
 
 app = Flask(__name__)
 
-if __name__ =="__main__":
+
+def add_patient_to_db(name, id, age):
+    new_patient = {"name": name, "id": id, "age": age}
+    db.append(new_patient)
+    return True
+
+
+def init_database():
+    add_patient_to_db("Ann Ables", 101, 35)
+    add_patient_to_db("Bob Boyles", 102, 40)
+
+
+if __name__ == "__main__":
+    init_database()
     app.run()
