0db47d94b3ae8750aa1fbc893650238b2a604352
Aimee McVey <aimee.mcvey@duke.edu>
2020-03-27 17:46:46 -0400

    Add remote server URL

--- health_db_client.py
+++ health_db_client.py
@@ -1,7 +1,8 @@
 # health_db_client.py
 import requests
 
-server_name = "http://127.0.0.1:5000"
+# server_name = "http://127.0.0.1:5000"
+server_name = "http://vcm-13874.vm.duke.edu:5000"
 
 
 def add_some_patients():
